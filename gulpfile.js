31045fdc65f404bfb2e9101502e46a4a9592c260
Nurbekkg <avtandilov06@gmail.com>
2017-08-24 18:51:56 +0600

    CHEK-34  Configuration gulp in project

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        js: 'web-application/staticfiles/js',
+        css: 'web-application/staticfiles/css/'
+    },
+    src: {
+        js: 'web-application/gulp/src/js/main.js',
+        style: 'web-application/gulp/src/style/main.sass'
+    },
+    watch: {
+        js: 'web-application/gulp/src/js/**/*.js',
+        style: 'web-application/gulp/src/style/**/*.sass'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        // .pipe(sourcemaps.init())
+        .pipe(uglify())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style)
+        // .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['src/style/'],
+            outputStyle: 'compressed',
+            // sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+
+
+gulp.task('build', [
+    'js:build',
+    'style:build'
+]);
+
+
+gulp.task('watch', function(){
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+});
+
+
+gulp.task('default', ['build', 'watch']);
---
+++ web-application/gulp/src/js/main.js
@@ -0,0 +1,138 @@
+//= ../../../staticfiles/bower_components/jquery/dist/jquery.js
+//= ../../../staticfiles/bower_components/selectize/dist/js/standalone/selectize.js
+
+
+$(function() {
+
+	// $('.tabs-stage > div').hide();
+	// $('.tabs-stage > div:nth-child(1)').show();
+	// $('.tabs-nav > li:first-child a').addClass('tab-active');
+
+	// $('.tabs-nav > li > a').on('click', function (event) {
+	// 	event.preventDefault();
+	// 	$('.tabs-nav > li a').removeClass('tab-active');
+	// 	$(this).addClass('tab-active');
+	// 	$('.tabs-stage > div').hide(400);
+	// 	$($(this).attr('href')).show(400);
+	// });
+
+
+	$('.authorised_user > li:last-child').click(function (event) {
+		event.stopPropagation();
+		var dropBlock = $('.sub_menu');
+		if (dropBlock.is(':hidden')) {
+			dropBlock.slideDown(250);
+		} else {
+			dropBlock.slideUp(250);
+		}
+	});
+
+
+	$(document).click( function() {
+		// $('.filters').slideUp();
+      $('.modal').fadeOut(350);
+      $('.blur').css('filter', 'none');
+      $('.registered_person').fadeOut(250);
+      $('.book').fadeOut(250);
+      $('.sub_menu').slideUp(250);
+    });
+
+	$('#log-in, .r_user').click(function (event) {
+			event.stopPropagation();
+			$('.modal').css('display', 'none');
+			$('.c_modal').fadeIn(350);
+			$('.blur').css('filter', 'blur(5px)');
+	});
+
+	$('#sign-up, .n_user').click(function (event) {
+			event.stopPropagation();
+			$('.modal').fadeIn(350);
+			$('.c_modal').css('display', 'none');
+			$('.logout_modal').css('display', 'none');
+			$('.blur').css('filter', 'blur(5px)');
+	});
+
+	$('.log-out').click(function (event) {
+			$('.modal').css('display', 'none');
+			$('.logout_modal').fadeIn(350);
+			// $('.logout_modal').css('display', 'none');
+			$('.blur').css('filter', 'blur(5px)');
+	});
+
+	$('.s_register, .c_register, .registered_person, .book, .logout_modal > div').click(function (event) {
+			event.stopPropagation();
+	});
+
+	var password = document.getElementById("password")
+	, confirm_password = document.getElementById("confirm_password");
+
+	function validatePassword(){
+		if(password.value != confirm_password.value) {
+			confirm_password.setCustomValidity("Пароли не совпадают");
+		} else {
+			confirm_password.setCustomValidity('');
+		}
+	}
+
+	password.onchange = validatePassword;
+	confirm_password.onkeyup = validatePassword;
+
+
+	function preview(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function (e) { 
+				$('#img').attr('src', e.target.result);  
+			}
+			reader.readAsDataURL(input.files[0]);     
+		}   
+	}
+
+	$("#upload").change(function(){
+		preview(this);
+	});
+
+	var r_person = $('.registered_person');
+	$('.dt li.registered').append(r_person);
+
+
+	var r_cell = $('.dt li.registered');
+
+	r_cell.each(function(i, obj) {
+		$(obj).on('click', function(event) {
+			event.stopPropagation();
+			$('.book').fadeOut(250);
+			$('.registered_person').fadeOut(250);
+			$(this).children('.registered_person').fadeIn(250);
+		});
+	});
+
+	var booking = $('.book');
+	$('.dt li.free').append(booking);
+	var f_cell = $('.dt li.free');
+	f_cell.each(function(i, obj) {
+		$(obj).on('click', function(event) {
+			event.stopPropagation();
+			$('.registered_person').fadeOut(250);
+			$('.book').fadeOut(250);
+			$(this).children('.book').fadeIn(250);
+		});
+	});
+
+
+	$('#place').selectize({
+		sortField: 'text'
+	});
+
+	$('.add_block > span').click(function (event) {
+		$(this).siblings('div').append('<input type="tel" placeholder="Контактный телефон">');
+	});
+
+
+	$('.no').click(function (event) {
+			$('.logout_modal').fadeOut(250);
+			$('.blur').css('filter', 'none');
+	});
+
+
+});
