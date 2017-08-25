87fda27591443959b632e85438ea14969d6706e7
kayrat <kairatomurbek2@gmail.com>
2017-08-26 01:07:37 +0600

    CHEK-2 Реализовал пагинацию в списке учреждений

--- web-application/webapp/views/company.py
+++ web-application/webapp/views/company.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import Http404
 from django.views.generic import ListView
 from django.views.generic import TemplateView
 
@@ -19,11 +21,27 @@ class CompanyList(ListView):
     template_name = 'company/company_list.html'
     filterset_class = CompanyFilter
     model = Company
+    paginate_by = 1
+
+    def get_queryset(self):
+        if self.request.is_ajax():
+            self.template_name = 'company/object.html'
+        return Company.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super(CompanyList, self).get_context_data(**kwargs)
-        company_filter = self.filterset_class(self.request.GET, queryset=Company.objects.all())
-        context['company_filter'] = company_filter
+        company_filter = self.filterset_class(self.request.GET, queryset=self.get_queryset())
+        context['company_filter'] = company_filter.form
+        pagination = Paginator(company_filter.qs, self.paginate_by)
+        page = self.request.GET.get('page')
+        try:
+            company_list = pagination.page(page)
+        except PageNotAnInteger:
+            company_list = pagination.page(1)
+        except EmptyPage:
+            raise Http404("That page contains no results")
+        context['company_list'] = company_list.object_list
+        context['is_paginated'] = company_list.has_next()
         return context
 
 
