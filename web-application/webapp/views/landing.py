c7f77d3da9dd4e64852926253e27342efcc4dc5f
kayrat <kairatomurbek2@gmail.com>
2017-09-12 11:16:43 +0600

    CHEK-6 Реализовал результат поиска

--- web-application/webapp/views/landing.py
+++ web-application/webapp/views/landing.py
@@ -1,6 +1,7 @@
 from dal import autocomplete
 from django.views.generic import TemplateView
 from taggit.models import Tag
+from webapp.models import Company, Specialist
 
 
 class HomeView(TemplateView):
@@ -17,4 +18,26 @@ class TagAutocomplete(autocomplete.Select2QuerySetView):
         if self.q:
             qs = qs.filter(name__istartswith=self.q)
 
+        return qs
+
+
+class SearchView(TemplateView):
+    template_name = 'general/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        context['companies'] = self._get_company()
+        context['masters'] = self._get_specialist()
+        return context
+
+    def _get_company(self):
+        qs = Company.objects.all()
+        if self.request.GET['q']:
+            qs = qs.filter(name__icontains=self.request.GET['q'])
+        return qs
+
+    def _get_specialist(self):
+        qs = Specialist.objects.all()
+        if self.request.GET['q']:
+            qs = qs.filter(full_name__icontains=self.request.GET['q'])
         return qs
