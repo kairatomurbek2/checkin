18c5782610ece1cb46f4f11d9b05d89b86f7233c
SolArt <hound2014@gmail.com>
2017-11-03 17:19:30 +0600

    fixed bug with checkboxes in categories

--- web-application/gulp/src/js/get-params.js
+++ web-application/gulp/src/js/get-params.js
@@ -1,12 +1,20 @@
 function getParameter(variable) {
     var param = [];
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable) {
-            param.push(pair[1]);
+
+    var query = window.location.href;
+    if (query.indexOf("?") !== -1) {
+        query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) {
+                param.push(pair[1]);
+            }
         }
+        return (param);
+    } else {
+        query = window.location.pathname.split('/')[2];
+        param.push(query);
+        return (param);
     }
-    return (param);
 }
