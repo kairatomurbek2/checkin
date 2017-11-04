09de238e1322ff15c8c776ecc77a9aff13be215e
Nurbekkg <avtandilov06@gmail.com>
2017-11-04 13:29:43 +0600

    Исправил ошибку в файле main.js

--- web-application/gulp/src/js/main.js
+++ web-application/gulp/src/js/main.js
@@ -109,20 +109,6 @@ $(function () {
         event.stopPropagation();
     });
 
-    var password = document.getElementById("password")
-        , confirm_password = document.getElementById("confirm_password");
-
-    function validatePassword() {
-        if (password.value != confirm_password.value) {
-            confirm_password.setCustomValidity("Пароли не совпадают");
-        } else {
-            confirm_password.setCustomValidity('');
-        }
-    }
-
-    password.onchange = validatePassword;
-    confirm_password.onkeyup = validatePassword;
-
 
     function preview(input) {
         if (input.files && input.files[0]) {
