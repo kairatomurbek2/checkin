15d74886d4d4d5d841d1e7255a0a63f82fc87785
SolArt <hound2014@gmail.com>
2017-11-20 00:55:55 +0600

    added support of multiple companies

--- web-application/gulp/src/js/calendar/calendar.js
+++ web-application/gulp/src/js/calendar/calendar.js
@@ -10,6 +10,7 @@ let app = new Vue({
         record: {},
         reservations: null,
         mainArray: [],
+        companies: [],
         _masterSlug: '',
         _masterUser: '',
         _csrfToken: '',
@@ -21,7 +22,7 @@ let app = new Vue({
     },
 
     methods: {
-        getDataFromDjango(val, slug, csrfToken){
+        getDataFromDjango(val, slug, csrfToken) {
             this._masterSlug = slug;
             this._masterUser = val;
             this._csrfToken = csrfToken;
@@ -30,7 +31,6 @@ let app = new Vue({
         getMasterSchedule() {
             this.$http.get('/api/schedule-setting/' + this._masterSlug).then(response => {
                 this.schedule = response.body;
-                console.log(this.schedule);
                 this.getMasterOrders();
             }, error => {
                 console.error(error);
@@ -55,27 +55,41 @@ let app = new Vue({
                         if (typeof daySchedule.interval !== 'object') {
                             this.getWorkInterval(daySchedule, 'interval');
                         }
-                        this.addDatesToArray(new Date(now), daySchedule);
-                        this.mainArray.push(this.dayTime);
-                        now.setDate(now.getDate() + 1);
+                        this.addDatesToArray(new Date(now), daySchedule, schedule.company);
+                        let result = this.checkIfInMainArrayDateExists(now);
+                        if (result.status) {
+                            this.mainArray[result.index].times = this.mainArray[result.index].times.concat(this.dayTime.times);
+                        } else {
+                            this.mainArray.push(this.dayTime);
+                        }
+
                     } else {
-                        this.mainArray.push({
-                            date: new Date(now),
-                            times: [
-                                {
-                                    time: new Date(now),
-                                    status: 'holiday'
-                                }
-                            ]
-                        });
-                        now.setDate(now.getDate() + 1);
+                        let result = this.checkIfInMainArrayDateExists(now);
+                        if (result.status) {
+                            // check if holiday already added to array (need to check if holiday is first in array)
+                            if (this.mainArray[result.index].length > 1) {
+                                this.mainArray[result.index].times.push({time: new Date(now), status: 'holiday'});
+                            }
+                        } else {
+                            this.mainArray.push({
+                                date: new Date(now),
+                                times: [
+                                    {
+                                        time: new Date(now),
+                                        status: 'holiday'
+                                    }
+                                ]
+                            })
+                        }
                     }
-                })
+                });
+                now.setDate(now.getDate() + 1);
             }
-            console.log('-------MAIN ARRAY--------');
+            // when dates were added from different schedules, it's needed to sort it all
+            this.sortTimesInMainArray();
             console.log(this.mainArray);
         },
-        addDatesToArray(now, daySchedule) {
+        addDatesToArray(now, daySchedule, companyId) {
             let oneDay = {
                 initialDate: new Date(now),
                 date: new Date(now)
@@ -88,6 +102,7 @@ let app = new Vue({
                     daySchedule = this.getWorkInterval(daySchedule, 'lunch_settings');
                     let time = this.doSetIntervalLimit(daySchedule, oneDay, 'lunch_settings');
                     if (time) {
+                        time.company = companyId;
                         time.status = 'lunch';
                         times.push(time);
                         oneDay.date.setHours(daySchedule.lunch_settings.end.hours, daySchedule.lunch_settings.end.minutes, 0, 0);
@@ -98,29 +113,33 @@ let app = new Vue({
                     daySchedule = this.getWorkInterval(daySchedule, 'live_recording');
                     let time = this.doSetIntervalLimit(daySchedule, oneDay, 'live_recording');
                     if (time) {
+                        time.company = companyId;
                         time.status = 'live';
                         times.push(time);
                         oneDay.date.setHours(daySchedule.live_recording.end.hours, daySchedule.live_recording.end.minutes, 0, 0);
                         continue;
                     }
                 }
-                if (oneDay.date.getTime() <= new Date().getTime()) {
-                    times.push({
-                        time: new Date(oneDay.date),
-                        status: 'left'
-                    })
-                    oneDay.date.setHours(oneDay.date.getHours() + daySchedule.interval.hours);
-                    oneDay.date.setMinutes(oneDay.date.getMinutes() + daySchedule.interval.minutes);
-                    continue;
-                }
+                // check if record is out of real time
+                // if (oneDay.date.getTime() <= new Date().getTime()) {
+                //     times.push({
+                //         time: new Date(oneDay.date),
+                //         status: 'left'
+                //     });
+                //     oneDay.date.setHours(oneDay.date.getHours() + daySchedule.interval.hours);
+                //     oneDay.date.setMinutes(oneDay.date.getMinutes() + daySchedule.interval.minutes);
+                //     continue;
+                // }
                 let record = this.orderFreeRecords(oneDay.date);
                 if (record) {
+                    record.company = companyId;
                     times.push(record);
                     oneDay.date.setHours(oneDay.date.getHours() + daySchedule.interval.hours);
                     oneDay.date.setMinutes(oneDay.date.getMinutes() + daySchedule.interval.minutes);
                     continue;
                 }
                 times.push({
+                    company: companyId,
                     time: new Date(oneDay.date),
                     status: 'free'
                 });
@@ -129,11 +148,11 @@ let app = new Vue({
             }
             this.dayTime = {date: oneDay.date, times: times};
         },
-        doCorrectDateInOrders(){
+        doCorrectDateInOrders() {
             this.reservations.map(order => {
                 let date = {
-                    date: order.date_time_reservation.split(" ")[0].split("."),
-                    time: order.date_time_reservation.split(" ")[1].split(':'),
+                    date: order.date_time_reservation.split(' ')[0].split('.'),
+                    time: order.date_time_reservation.split(' ')[1].split(':'),
                     jsDate: ''
                 };
                 date.jsDate = new Date('' + date.date[1] + '-' + date.date[0] + '-' + date.date[2]).setHours(parseInt(date.time[0]), parseInt(date.time[1]), 0, 0);
@@ -201,25 +220,25 @@ let app = new Vue({
             });
             return record;
         },
-        prettyDate(date, param){
+        prettyDate(date, param) {
             let res = '';
             date = new Date(date);
             if (param === 'date') {
-                res += date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear();
+                res += date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
                 return res;
             }
             if (param === 'time') {
-                res += (date.getHours() > 9 ? date.getHours() : '0'+ date.getHours()) + ':' +
+                res += (date.getHours() > 9 ? date.getHours() : '0' + date.getHours()) + ':' +
                     (date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes());
                 return res;
             }
         },
-        prettyDateRange(dates, param){
+        prettyDateRange(dates, param) {
             let res1 = this.prettyDate(dates.start, param);
             let res2 = this.prettyDate(dates.end, param);
             return '' + res1 + ' - ' + res2;
         },
-        setOrderStatusInArray(time){
+        setOrderStatusInArray(time) {
             this.mainArray.forEach(day => {
                 if (day.date.getDate() === time.time.getDate()) {
                     day.times.forEach(timeEl => {
@@ -238,41 +257,40 @@ let app = new Vue({
         },
         toggleOrderPopup(time) {
             if (time.status === 'free' || time.status === 'refused') {
-                console.log(document.querySelector(".order-modal-wrap"));
                 this.record = time;
-                document.querySelector(".blur").style.filter = "blur(5px)";
-                document.querySelector(".order-modal-wrap").style.display = 'block';
-                document.querySelector(".title-time").innerHTML = "Запись на " + this.prettyDate(time.time, 'date') + ' ' + this.prettyDate(time.time, 'time');
-                
+                document.querySelector('.blur').style.filter = 'blur(5px)';
+                document.querySelector('.order-modal-wrap').style.display = 'block';
+                document.querySelector('.title-time').innerHTML = 'Запись на ' + this.prettyDate(time.time, 'date') + ' ' + this.prettyDate(time.time, 'time');
+
             }
             if (!this._masterUser) {
                 if (time.status === 'armored') {
-                    $("#armored-modal").modal();
+                    $('#armored-modal').modal();
                 }
-            } 
+            }
         },
         makeOrder() {
-            if (!document.querySelector("#phone").value) {
-                document.querySelector(".error").innerHTML += '<div> Поле телефон не введено </div>';
-                document.querySelector(".error").style.display = "block";
-                input = document.querySelector("#phone");
+            if (!document.querySelector('#phone').value) {
+                document.querySelector('.error').innerHTML += '<div> Поле телефон не введено </div>';
+                document.querySelector('.error').style.display = 'block';
+                input = document.querySelector('#phone');
                 input.classList.add('error');
-                setTimeout(function(){
+                setTimeout(function () {
                     input.classList.remove('error');
-                    document.querySelector(".error").innerHTML = "";
-                    document.querySelector(".error").style.display = "";
+                    document.querySelector('.error').innerHTML = '';
+                    document.querySelector('.error').style.display = '';
                 }, 3000);
                 return;
             }
-            this.record.name = document.querySelector("#full_name").value;
-            this.record.phone = document.querySelector("#phone").value;
+            this.record.name = document.querySelector('#full_name').value;
+            this.record.phone = document.querySelector('#phone').value;
             let url = '/api/reservation/add/' + this._masterSlug + '/';
             let body = {
                 full_name: this.record.name,
                 phone: this.record.phone,
-                date_time_reservation: ''+this.record.time.getFullYear()+'-'+(this.record.time.getMonth()+1)+'-'+this.record.time.getDate()+
-                ' '+(this.record.time.getHours()>9?this.record.time.getHours():'0'+this.record.time.getHours())+
-                ':'+(this.record.time.getMinutes()>9?this.record.time.getMinutes():'0'+this.record.time.getMinutes()),
+                date_time_reservation: '' + this.record.time.getFullYear() + '-' + (this.record.time.getMonth() + 1) + '-' + this.record.time.getDate() +
+                ' ' + (this.record.time.getHours() > 9 ? this.record.time.getHours() : '0' + this.record.time.getHours()) +
+                ':' + (this.record.time.getMinutes() > 9 ? this.record.time.getMinutes() : '0' + this.record.time.getMinutes()),
                 status: 'armored'
             };
             if (this._masterUser) {
@@ -282,39 +300,40 @@ let app = new Vue({
                 headers: {
                     'X-CSRFToken': this._csrfToken
                 }
-            }
+            };
             this.$http.post(url, body, options).then(response => {
-                if (response.body.status != 'error') {
-                    document.querySelector(".success").style.display = "block";
-                    document.querySelector(".success").innerHTML = "Заявка успешно оформлена, ожидайте подтверждение специалиста";
+                if (response.body.status !== 'error') {
+                    document.querySelector('.success').style.display = 'block';
+                    document.querySelector('.success').innerHTML = 'Заявка успешно оформлена, ожидайте подтверждение специалиста';
                     this.record.status = 'armored';
                     if (this._masterUser) {
                         this.record.status = 'confirmed';
-                        document.querySelector(".success").innerHTML = "Заявка успешно оформлена";
+                        document.querySelector('.success').innerHTML = 'Заявка успешно оформлена';
                     }
                     this.record.id = response.body.id;
                     this.setOrderStatusInArray(this.record);
                     this.record.date_time_reservation = this.record.time;
                     this.reservations.push(this.record);
-                    setTimeout(function(){
-                        document.querySelector(".success").innerHTML = "";
-                        document.querySelector(".success").style.display = "";
-                        document.querySelector(".error").innerHTML = "";
+                    setTimeout(function () {
+                        document.querySelector('.order-modal-wrap').style.display = 'none';
+                        document.querySelector('.success').innerHTML = '';
+                        document.querySelector('.success').style.display = '';
+                        document.querySelector('.error').innerHTML = '';
                         this.cleanOrderPopup();
                     }, 3000);
                 } else {
-                    document.querySelector(".error").style.display = 'block';
-                    document.querySelector(".error").innerHTML += '<div class="authorization-text">' + response.body.message + '</div>';
-                    document.querySelector(".error").insertAdjacentHTML('afterend', '<div class="authorization-text" style="text-align: center"><a href="/accounts/login">Авторизация</a></div>')
+                    document.querySelector('.error').style.display = 'block';
+                    document.querySelector('.error').innerHTML += '<div class="authorization-text">' + response.body.message + '</div>';
+                    document.querySelector('.error').insertAdjacentHTML('afterend', '<div class="authorization-text" style="text-align: center"><a href="/accounts/login">Авторизация</a></div>')
                 }
             }, response => {
                 console.error(response);
             })
         },
-        cleanOrderPopup(){
-            document.querySelector(".order-modal-wrap").style.display = 'none';
-            document.querySelector(".blur").style.filter = "blur(0px)";
-            document.querySelector(".title-time").innerHTML = "";
+        cleanOrderPopup() {
+            document.querySelector('.order-modal-wrap').style.display = 'none';
+            document.querySelector('.blur').style.filter = 'blur(0px)';
+            document.querySelector('.title-time').innerHTML = '';
         },
         setRecordStatus(time, status) {
             if (this._masterUser) {
@@ -330,12 +349,12 @@ let app = new Vue({
                 this.$http.put(url, body, options).then(response => {
                     if (response.body.status === 'refused') {
                         time.status = 'free';
-                        $("#confirm-modal .modal-body").html("Завяка успешно отклонена");
-                        $("#confirm-modal").modal();
+                        $('#confirm-modal .modal-body').html('Завяка успешно отклонена');
+                        $('#confirm-modal').modal();
                     } else {
                         time.status = 'confirmed';
-                        $("#confirm-modal .modal-body").html("Завяка успешно одобрена");
-                        $("#confirm-modal").modal();
+                        $('#confirm-modal .modal-body').html('Завяка успешно одобрена');
+                        $('#confirm-modal').modal();
                     }
                     this.setOrderStatusInArray(time);
                 }, error => {
@@ -356,7 +375,7 @@ let app = new Vue({
         },
         moveToPreviousWeek() {
             this.mainArray = [];
-            let start = new Date(this.range.current).setDate(new Date(this.range.current).getDate() - (this.period*2));
+            let start = new Date(this.range.current).setDate(new Date(this.range.current).getDate() - (this.period * 2));
             let end = new Date(this.range.current).setDate(new Date(this.range.current).getDate() - this.period);
             if (new Date(start).getTime() <= new Date(this.range.start).getTime()) {
                 start = new Date(this.range.start);
@@ -364,6 +383,50 @@ let app = new Vue({
             }
             this.range.current = end;
             this.fillRange(start, end);
+        },
+        checkIfInMainArrayDateExists(dateObject) {
+            if (dateObject instanceof Object && dateObject.date) {
+                dateObject = dateObject.date;
+            }
+            let result = this.mainArray.findIndex(x => {
+                return new Date(x.date.setHours(0,0,0,0)).getTime() === new Date(dateObject).getTime()
+            });
+            if (result !== -1) {
+                return {status: true, index: result}
+            } else {
+                return {status: false, index: -1};
+            }
+        },
+        sortTimesInMainArray() {
+            this.mainArray.forEach(item => {
+                // debugger;
+
+                if (item.times.length > 1 && item.times[0].status === 'holiday') {
+                    item.times.splice(0, 1);
+                }
+
+                item.times.sort((a, b) => {
+                    if (a.time.start) {
+                        return new Date(a.time.start) - new Date(b.time);
+                    }
+                    if (b.time.start) {
+                        return new Date(a.time) - new Date(b.time.start);
+                    }
+                    if (a.time.start && b.time.start) {
+                        return new Date(a.time.start) - new Date(b.time.start);
+                    }
+                    return new Date(a.time) - new Date(b.time);
+                });
+            })
+        },
+        makeClassFromCompany(time){
+            let index = this.companies.indexOf(time.company);
+            if (index !== -1) {
+                return 'company-' + index
+            } else {
+                this.companies.push(time.company);
+                return 'company-' + (this.companies.length - 1);
+            }
         }
     }
 });
