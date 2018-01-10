3d5b25505b881b194836dde6b86fc94fb1aca2e4
kayrat <kairatomurbek2@gmail.com>
2018-01-10 15:21:41 +0600

    #CHEK-172 Реализовал token_key для web

--- web-application/webapp/adapters.py
+++ web-application/webapp/adapters.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from rest_framework.authtoken.models import Token
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -10,6 +11,11 @@ class AccountAdapter(DefaultAccountAdapter):
     def get_login_redirect_url(self, request):
         path = "/"
         return path
+
+    def login(self, request, user):
+        super(AccountAdapter, self).login(request, user)
+        token, created = Token.objects.get_or_create(user=request.user)
+        request.session['token_key'] = token.key
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
