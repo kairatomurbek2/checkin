d3f74ebe69dac07166372cf1a87a4e3ee98b3f34
kayrat <kairatomurbek2@gmail.com>
2018-01-10 17:29:26 +0600

    #CHEK-172 Add rest-auth/registration

--- web-application/api_mob/urls.py
+++ web-application/api_mob/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'companies/(?P<company_slug>[-_\w]+)/reviews/$', CompanyReviewsListApi.as_view(),
         name='company_reviews_api_v1'),
     url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
     url(r'^rest-auth/google/$', GoogleLogin.as_view(), name='google_login'),
 ]
