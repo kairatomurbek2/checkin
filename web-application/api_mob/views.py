f55f25d87c8d2edea958cac47f925733e1e6b3c5
kayrat <kairatomurbek2@gmail.com>
2018-01-24 14:35:53 +0600

    CHEK-172 Удалил закомеченный код

--- web-application/api_mob/views.py
+++ web-application/api_mob/views.py
@@ -216,30 +216,3 @@ class ProfileFavoriteListViewApi(generics.ListAPIView):
 
 class UserDetailsViewApi(UserDetailsView):
     authentication_classes = (TokenAuthentication,)
-
-
-# class UserDetailsViewApi(RetrieveUpdateAPIView):
-#     """
-#     Reads and updates UserModel fields
-#     Accepts GET, PUT, PATCH methods.
-#
-#     Default accepted fields: username, first_name, last_name
-#     Default display fields: pk, username, email, first_name, last_name
-#     Read-only fields: pk, email
-#
-#     Returns UserModel fields.
-#     """
-#     serializer_class = UserDetailsSerializer
-#     permission_classes = (IsAuthenticated,)
-#     authentication_classes = (TokenAuthentication,)
-#
-#     def get_object(self):
-#         return self.request.user
-#
-#     def get_queryset(self):
-#         """
-#         Adding this method since it is sometimes called when using
-#         django-rest-swagger
-#         https://github.com/Tivix/django-rest-auth/issues/275
-#         """
-#         return get_user_model().objects.none()
