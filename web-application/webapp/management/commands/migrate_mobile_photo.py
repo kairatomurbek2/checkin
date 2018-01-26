86f485aa892cde5aa6982129a9b3f00e709c42ac
kayrat <kairatomurbek2@gmail.com>
2018-01-26 18:37:45 +0600

    Сделал проверку на компании

--- web-application/webapp/management/commands/migrate_mobile_photo.py
+++ web-application/webapp/management/commands/migrate_mobile_photo.py
@@ -5,6 +5,6 @@ from webapp.models import Specialist
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for specialist in Specialist.objects.all():
+        for specialist in Specialist.all_objects.all():
             specialist.mobile_photo = specialist.photo
             specialist.save()
--- web-application/webapp/management/commands/resize_mobile_photo.py
+++ web-application/webapp/management/commands/resize_mobile_photo.py
@@ -7,7 +7,7 @@ from webapp.models import Specialist
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for specialist in Specialist.objects.all():
+        for specialist in Specialist.all_objects.all():
             resized = get_thumbnail(specialist.mobile_photo, "150x150")
             specialist.mobile_photo.save(resized.name, ContentFile(resized.read()), True)
             specialist.save()
