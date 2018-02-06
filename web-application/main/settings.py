51e96ac260de1d58890217c5be0513029cd22c4e
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-02-06 12:46:43 +0600

    ^ changed ALLOWED_HOSTS constant value

--- web-application/main/settings.py
+++ web-application/main/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'f1p+^!5dp@dcy-sfzugpgmkx($pw^9h^g)6%n)#^=u3*$y$c4s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
