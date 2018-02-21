a631511521326b26dcdce5fb7f57edcbe168eb76
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-02-21 14:16:42 +0600

    + added FIREBASE consts to settings

--- web-application/webapp/signals.py
+++ web-application/webapp/signals.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 from webapp.models import Company, CompanyContact, Reservation
 from webapp.firebase import FirebaseHelper
 
+from main.settings_local import FIREBASE_URL, FIREBASE_SECRET, FIREBASE_USER_EMAIL
+
 
 @receiver(post_save, sender=Company)
 def create_company_phone(sender, instance, created, **kwargs):
@@ -14,8 +16,8 @@ def create_company_phone(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Reservation)
 def create_or_change_reservation(sender, instance, created, **kwargs):
-    firebase_helper = FirebaseHelper(firebase_url='', firebase_secret='',
-                                     firebase_email='')
+    firebase_helper = FirebaseHelper(firebase_url=FIREBASE_URL, firebase_secret=FIREBASE_SECRET,
+                                     firebase_email=FIREBASE_USER_EMAIL)
 
     if created:
         specialist_user = instance.specialist.user
