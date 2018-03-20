3cc6a57033c280906fade688bdfd64cf4783941c
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-03-20 14:02:31 +0600

    ^ disabled session auth in rest

--- web-application/main/settings.py
+++ web-application/main/settings.py
@@ -222,7 +222,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
